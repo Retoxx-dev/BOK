14dee8a7ea915560a18e3c240e6404b8c7ee78d9
xGanczix <95712763+xGanczix@users.noreply.github.com>
2021-12-13 19:15:54 +0100

    Add files via upload

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BOK.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
