117ba88b570d328524a2f4a1977a8801d7a9c580
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2021-12-22 21:51:33 +0100

    Added login logic

---
+++ Models/SeedContext.cs
@@ -0,0 +1,33 @@
+﻿using BOK.Data;
+using BOK.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BOK.Models
+{
+    public class SeedContext
+    {
+
+            public static async Task SeedSuperAdmin(UserManager<ApplicationUser> userManager)
+            {
+                //Seed Default User
+                var defaultUser = new ApplicationUser
+                {
+                    UserName = "a@admin.com",
+                    Email = "a@admin.com",
+                    FirstName = "Super Admin"
+                };
+                if (userManager.Users.All(u => u.Id != defaultUser.Id))
+                {
+                    var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                    if (user == null)
+                    {
+                        await userManager.CreateAsync(defaultUser, "zaq1@WSX");
+                    }
+                }
+            }
+    }
+}
--- README.md
+++
@@ -1,6 +0,0 @@
-# BOK
-
-# Database
-To start the database in this project, open Package Manager Console and put in:
-1. Add-Migration InitCreate
-2. Update-Database
