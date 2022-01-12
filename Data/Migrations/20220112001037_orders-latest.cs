1f79d64c5fd6567513e518a1f2d79fc77780616b
michal-wsei <michal.garncarz@microsoft.wsei.edu.pl>
2022-01-12 01:13:49 +0100

    add correct orders-latest migration

---
+++ Data/Migrations/20220112001037_orders-latest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BOK.Data.Migrations
+{
+    public partial class orderslatest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                schema: "Identity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OfferId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_Offer_OfferId",
+                        column: x => x.OfferId,
+                        principalSchema: "Identity",
+                        principalTable: "Offer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_OfferId",
+                schema: "Identity",
+                table: "Order",
+                column: "OfferId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order",
+                schema: "Identity");
+        }
+    }
+}
